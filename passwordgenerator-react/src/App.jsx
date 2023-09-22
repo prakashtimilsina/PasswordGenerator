a938621433798766c3f9a412b1b431f26b9dce8f
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-09-22 18:02:58 -0500

    Update App.jsx

--- passwordgenerator-react/src/App.jsx
+++ passwordgenerator-react/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
      <h1 className='text-3xl font-bold underline'>
-      Hello Prakash Timilsina.
+      Hello Prakash Timilsina. Creating the project - password generator!!
      </h1>
     </>
   )
