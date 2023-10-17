906ce09a263fa0575ce8b8f4c194729cbf99b78f
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-10-16 23:18:51 -0500

    length is completed.

--- passwordgenerator-react/src/App.jsx
+++ passwordgenerator-react/src/App.jsx
@@ -1,36 +1,56 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback } from "react";
 
 function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
-  const [length, setLength] = useState(8)
+  const [length, setLength] = useState(8);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("");
 
   // useCallBack - to use fn in memory.
-  const passwordGenerator = useCallback(()=>{
-    let passwd = ''
-    let strg = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    if(numberAllowed) strg += "0123456789"
-    if(charAllowed) strg += "!@#$%^&*()-=+~`{}[]"
+  const passwordGenerator = useCallback(() => {
+    let passwd = "";
+    let strg = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) strg += "0123456789";
+    if (charAllowed) strg += "!@#$%^&*()-=+~`{}[]";
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random()* strg.length + 1 )
-      
-      
+      let char = Math.floor(Math.random() * strg.length + 1);
+      pass += strg.charAt(char);
     }
-    
-  }, [length, charAllowed, numberAllowed, setPassword])
-
-  
-
+    setPassword(pass);
+  }, [length, charAllowed, numberAllowed, setPassword]);
 
   return (
     <>
-     <h1 className='text-3xl font-bold text-center text-black bg-amber-400 '>
-      Hello, Prakash Timilsina. Creating the project - Password Generator!
-     </h1>
+      <div
+        className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-5 
+      my-8 text-orange-400 bg-gray-500"
+      >
+        <h1 className="text-white text-center font-bold my-3">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none bg-green-400 text-black px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <input type="range" min={6} max={80}
+          value={length}
+          className="cursor-pointer"
+          onChange={(e)=> {setLength(e.target.value)}}/>
+          <label htmlFor="">Length: {length}</label>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
