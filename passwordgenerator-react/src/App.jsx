926d427f828e4f02182070394442c96f79b36e1c
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-10-16 22:25:31 -0500

    Update App.jsx

--- passwordgenerator-react/src/App.jsx
+++ passwordgenerator-react/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-3xl font-bold'>
+     <h1 className='text-3xl font-bold text-center text-black'>
       Hello, Prakash Timilsina. Creating the project - Password Generator!
      </h1>
     </>
