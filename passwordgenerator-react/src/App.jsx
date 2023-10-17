11518c8c6621fc96bbb9d496293cb2aadb2b00eb
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-10-16 22:49:33 -0500

    password func

--- passwordgenerator-react/src/App.jsx
+++ passwordgenerator-react/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useCallback } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [length, setLength] = useState(8)
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("")
+
+  // useCallBack - to use fn in memory.
+  const passwordGenerator = useCallback(()=>{
+    let passwd = ''
+    let strg = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if(numberAllowed) strg += "0123456789"
+    if(charAllowed) strg += "!@#$%^&*()-=+~`{}[]"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random()* strg.length + 1 )
+      
+      
+    }
+    
+  }, [length, charAllowed, numberAllowed, setPassword])
+
+  
+
 
   return (
     <>
-     <h1 className='text-3xl font-bold text-center text-black'>
+     <h1 className='text-3xl font-bold text-center text-black bg-amber-400 '>
       Hello, Prakash Timilsina. Creating the project - Password Generator!
      </h1>
     </>
