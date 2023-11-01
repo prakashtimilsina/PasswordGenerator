9d9c789225f349d0e36501ef92ff1593c6d29685
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-11-01 14:33:59 -0500

    darkmode using context api is completed.

--- themeswitchecontextAPI/src/App.jsx
+++ themeswitchecontextAPI/src/App.jsx
@@ -1,23 +1,44 @@
-
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+
+  //actual change in Theme
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
 
 
   return (
-   
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* Theme Button */}
-                    </div>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* Theme Button */}
+          <ThemeBtn />
+        </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       {/* card */}
-                    </div>
-                </div>
-            </div>
-
-  )
+        <div className="w-full max-w-sm mx-auto">
+          {/* card */}
+          <Card />
+          </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
