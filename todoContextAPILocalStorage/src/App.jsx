4fa926319b7be37b1180021630f79248e53632a5
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-11-02 10:32:43 -0500

    todo using context in progress

--- todoContextAPILocalStorage/src/App.jsx
+++ todoContextAPILocalStorage/src/App.jsx
@@ -1,13 +1,61 @@
-
+import { useEffect } from "react";
+import { Todoprovider } from "./context";
 
 function App() {
-  
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prevTodo) =>
+      prevTodo.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  //Local Storage
+  useEffect(()=>{
+    JSON.parse(localStorage.getItem("todos"))
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
+
+
 
   return (
-    <>
-  <h1 className="text-center bg-green-600 font-bold"> This is todo with ContextAPI  </h1>
-    </>
-  )
+    <Todoprovider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </Todoprovider>
+  );
 }
 
-export default App
+export default App;
